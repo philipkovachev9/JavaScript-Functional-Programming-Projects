8024ac98abe3b79daaa9bea8324b1130e4ac8346
Philip Kovachev <pkova@softserveinc.com>
2019-02-20 16:17:30 +0200

    Model data can now be changed according to input

--- caloriecounter/src/Update.js
+++ caloriecounter/src/Update.js
@@ -1,6 +1,7 @@
-
 const MSGS = {
-  SHOW_FORM: 'SHOW_FORM'
+  SHOW_FORM: 'SHOW_FORM',
+  MEAL_INPUT: 'MEAL_INPUT:',
+  CALORIE_INPUT: 'CALORIE_INPUT'
 };
 
 export function showFormMsg(showForm) {
@@ -10,11 +11,33 @@ export function showFormMsg(showForm) {
   }
 }
 
+export function mealInputMsg(description) {
+  return {
+    type: MSGS.MEAL_INPUT,
+    description
+  }
+}
+
+export function caloriesInputMsg(calories) {
+  return {
+    type: MSGS.CALORIE_INPUT,
+    calories
+  }
+}
+
 function update(msg, model) {
   switch(msg.type) {
     case MSGS.SHOW_FORM: {
        const { showForm } = msg;
-      return { ...model, showForm, description: '', calories: 0}
+      return { ...model, showForm, description: '', calories: 0 }
+    }
+    case MSGS.MEAL_INPUT: {
+      const { description } = msg;
+      return { ...model, description};
+    }
+    case MSGS.CALORIE_INPUT: {
+      const { calories } = msg;
+      return { ...model, calories};
     }
   }
   return model;
