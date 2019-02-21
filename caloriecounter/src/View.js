0ceda9aa052180f4b4fbf359f8e204776bb6cdcf
Philip Kovachev <pkova@softserveinc.com>
2019-02-21 12:04:32 +0200

    New meals can be now saved in the model

--- caloriecounter/src/View.js
+++ caloriecounter/src/View.js
@@ -1,6 +1,6 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
-import { showFormMsg, mealInputMsg, caloriesInputMsg } from './Update';
+import { showFormMsg, mealInputMsg, caloriesInputMsg, saveMealMsg } from './Update';
 const { pre, div, h1, button, form, label, input } = hh(h);
 
 function fieldSet(labelText, inputValue, oninput) {
@@ -39,7 +39,11 @@ function formView(dispatch, model) {
   const { description, calories, showForm } = model;
   if(showForm) {
   return form( {
-    className: 'w-100 mv2'
+    className: 'w-100 mv2',
+    onsubmit: e => {
+      e.preventDefault();
+      dispatch(saveMealMsg);
+    }
   },
   [
    fieldSet('Meal', description, e => dispatch(mealInputMsg(e.target.value)) ),
